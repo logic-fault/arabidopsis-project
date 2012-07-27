5a360a33c1e7b3ce6c233eb5df9829e4d510f01b
brock a <brock.wright.anderson@gmail.com>
2012-07-27 13:19:04 -0500

    foundational library init/close code

--- CGIOverlapTSS/CGIOverlapTSS.c
+++ CGIOverlapTSS/CGIOverlapTSS.c
@@ -1,7 +1,44 @@
-#include "genometools.h"
+/*
+* @file
+* @author Brock Anderson <brock.wright.anderson@gmail.com>
+* @section LICENSE
+* Released to public domain without restriction
+*
+* @section DESCRIPTION
+* Find CpGI that overlap TSS, mark as such
+*
+*************************************************/
+#include "genometools.h"	
+#include <stdio.h>
 
+void usage(const char * name)
+{
+   printf("Usage: %s <fileName>\n", name);
+}
 
 int main(int argc, char ** argv)
 {
+    GtError *err;
+    GtNodeStream * in_stream;
+    GtGenomeNode * node;
+    int err_num;
+
+    if (argc != 2)
+    {
+       usage(argv[0]);
+       exit(1);
+    }
+
+    // initilaize genometools
+    gt_lib_init();
+    err = gt_error_new();
+
+    // open the gff3 file we are evaluating
+    in_stream = gt_gff3_in_stream_new_sorted(argv[1]);  
+    gt_gff3_in_stream_show_progress_bar((GtGFF3InStream *)in_stream);
+
+    // close genome tools
+    gt_error_delete(err);
+    gt_lib_clean();
     return 0;
 }
