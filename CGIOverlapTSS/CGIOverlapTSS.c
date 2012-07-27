08014598e2e58975625377e02634c8963f0d46fe
brock a <brock.wright.anderson@gmail.com>
2012-07-27 15:11:58 -0500

    Add explanation and psuedocode for how to do this

--- CGIOverlapTSS/CGIOverlapTSS.c
+++ CGIOverlapTSS/CGIOverlapTSS.c
@@ -7,9 +7,45 @@
 * @section DESCRIPTION
 * Find CpGI that overlap TSS, mark as such
 *
+*
+*
+* Scenarios:
+*
+* CpGI              <---------------------------->
+* genes     
+*  (+) contained               <-----|     
+*  (-) contained                       |----->
+*  (+) periphery                             <--------|
+*  (-) periphery |-------->
+*
+*
+*  When you read a CpGI:
+*     latest_CpGINode  <- node_pointer
+*     cpgi_start       <- node_pointer->start
+*     cpgi_end         <- node_pointe->end
+*
+*     // see if (-) periphery case satisfied
+*     if (in_range(latest_tss, cpgi_start, cpgi_end))
+*         mark(latest_CpGINode)  
+*         mark(latest_gene)  // tell which CpGINode also
+*       
+*
+*  When read gene: 
+*                  second_latest_tss <- latest_tss
+*                  latest_tss        <- gene's tss
+*                  latest_gene       <- gene reference
+*
+*                  // below case covers contained statements and
+*                  // (+) periphery
+*                  if (in_range(latest_tss,cpgi_start,cpgi_end)
+*                      mark(latest_CpGINode)
+*                      mark(latest_gene) // tell which CpGINode also
+                   
+
 *************************************************/
 #include "genometools.h"	
 #include <stdio.h>
+
 
 void usage(const char * name)
 {
@@ -21,6 +57,10 @@ int main(int argc, char ** argv)
     GtError *err;
     GtNodeStream * in_stream;
     GtGenomeNode * node;
+   
+    unsigned long latest_tss; // tss for the last entry gene we read
+    unsigned long second_latest_tss; // tss for the second to last entry we read
+
     int err_num;
 
     if (argc != 2)
@@ -35,8 +75,9 @@ int main(int argc, char ** argv)
 
     // open the gff3 file we are evaluating
     in_stream = gt_gff3_in_stream_new_sorted(argv[1]);  
-    gt_gff3_in_stream_show_progress_bar((GtGFF3InStream *)in_stream);
-
+      
+    // parse the file
+    
     // close genome tools
     gt_error_delete(err);
     gt_lib_clean();
