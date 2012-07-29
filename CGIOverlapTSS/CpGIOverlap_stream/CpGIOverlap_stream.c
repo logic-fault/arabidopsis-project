6ef4330c4d572c4952d5c69bba39ca9b865aa1d9
brock a <brock.wright.anderson@gmail.com>
2012-07-29 11:05:49 -0500

    Wireframe for the CpGIOverlap_stream class

--- CGIOverlapTSS/CpGIOverlap_stream/CpGIOverlap_stream.c
+++ CGIOverlapTSS/CpGIOverlap_stream/CpGIOverlap_stream.c
@@ -28,12 +28,49 @@ struct CpGIOverlap_stream {
     unsigned long latest_tss;
 };
 
-#define CpGIOverlap_stream_cast(GS)\
-        CpGIOverlap_stream_cast(CpGI_overlap_stream_class(), GS);
+#define CpGIOverlap_stream_cast(GS) CpGIOverlap_stream_cast(CpGI_overlap_stream_class(), GS);
 
+
+const GtNodeStreamClass * CpGIOverlap_stream_class(void)
+{
+    static const GtNodeStreamClass * c = NULL;
+    if (!c)
+    {
+        c = gt_node_stream_class_new( sizeof(CpGIOverlap_stream),
+                                      CpGIOverlap_stream_free,
+                                      CpGIOverlap_stream_next
+                                    );
+    }
+    
+    return c;
+}
+
+GtNodeStream * CpGIOverlap_stream_new(GtNodeStream * in_stream)
+{
+    GtNodeStream * ns = gt_node_stream_create(CpGIOverlap_stream_class(), 
+                                              true); // must be sorted
+    CpGIOverlap_stream * overlap_stream = CpGIOverlap_stream_cast(ns);
+    gt_assert(in_stream);
+    overlap_stream->in_stream = gt_node_stream_ref(in_stream);
+
+    overlap_stream->index = 0;
+
+    // we haven't found an island yet
+    overlap_stream->island.start = 0;
+    overlap_stream->island.end   = 0;
+    overlap_stream->latest_tss   = 0;
+
+    // haven't found a cpGI or gene yet
+    overlap_stream->latest_CpGI_node = NULL;
+    overlap_stream->latest_gene_node = NULL;
+
+
+ 
+}
 
 static int CpGIOverlap_stream_next(GtNodeStream * ns,
                                    GtGenomeNode ** gn,
                                    GtError * err)
 {
+    return 0;
 }
